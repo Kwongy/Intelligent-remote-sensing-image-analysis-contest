7d1dec1717a0bbc50bc1da03a2bea39e478c0eb4
Kwongy <760030764@qq.com>
2019-08-10 22:34:02 +0800

    主函数 训练模型

--- segmentation/experiment/segmentation/Train.py
+++ segmentation/experiment/segmentation/Train.py
@@ -8,12 +8,10 @@ import sys
 import torch
 from loss.loss import EdgeLoss
 from loss.loss import SegmentationLoss
-from script.utils import poly_lr_scheduler
 from tensorboardX import SummaryWriter
 from torch.autograd import Variable
 from torch.utils import data
 from experiment.segmentation.data_loader import loadTrainData
-from models.efficientnet_pytorch.change_detection import EfficientFPN
 from models.swiftnet import swiftnet
 sys.path.append("..")
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -34,14 +32,7 @@ def train(args):
     # Setup Model
     weight_dir = "C:\\Users\\kwong\\Downloads\\w19-40.pkl"
 
-    # for i, dataset in enumerate(trainloader):
-    #     print(dataset, type(dataset))
-    #     break
-    #
-    # return
-
     if finetune:
-        # model = EfficientFPN.from_name(args.arch)
         model = swiftnet(False, num_classes=16)
         weight = torch.load(weight_dir)
         model.load_state_dict(weight, strict=False)
@@ -52,7 +43,6 @@ def train(args):
                     print('freeze layer: %s' % name)
     else:
         model = swiftnet(True, num_classes=16)
-        # model = EfficientFPN.from_pretrained(args.arch)
     model.train()
     if torch.cuda.is_available():
         # model = DataParallelModel(model)
@@ -60,26 +50,14 @@ def train(args):
         model.float()
         model.cuda()
     ce_weight = 0
-    c_weight = [0.7, 2.0, 0.75, 0.8,
-                 2.0, 2., 2., 2.,
-                 2., 2., 0.8, 0.8,
-                 2., 2., 2., 2.]
     iou_weight = 1
-    # init class loss for semantic segmentation
-    class_criterion = SegmentationLoss(c_weight=c_weight, ce_weight=ce_weight, iou_weight=iou_weight)
-    # class_criterion = DataParallelCriterion(class_criterion)
-    # class_criterion.cuda()
+    class_criterion = SegmentationLoss(ce_weight=ce_weight, iou_weight=iou_weight)
 
-    # edge_criterion = EdgeLoss()
-    # edge_criterion = DataParallelCriterion(edge_criterion)
-    # edge_criterion.cuda()
     optimizer = torch.optim.SGD(model.parameters(), lr=args.l_rate, momentum=0.99, weight_decay=5e-4)
 
     writer = SummaryWriter(logdir=log_dir,
                            comment='fine tune on {}, lr {},{} epoch in total'.format(weight_dir, args.l_rate,
                                                                                      args.n_epoch))
-
-    # freeze edge detection to do fine tune
 
     for epoch in range(args.n_epoch):
         loss_sum = 0
@@ -91,8 +69,6 @@ def train(args):
             else:
                 images = Variable(images).float()
                 labels = Variable(labels).float()
-            iter = len(trainloader) * epoch + i
-            poly_lr_scheduler(optimizer, args.l_rate, iter)
             optimizer.zero_grad()
             predictions = model(images)
             loss = class_criterion(predictions, labels)
@@ -139,16 +115,8 @@ if __name__ == '__main__':
                         help='Learning Rate')
     parser.add_argument('--feature_scale', nargs='?', type=int, default=1,
                         help='Divider for # of features to use')
-    parser.add_argument('--pretrained_model', nargs='?', type=str,
-                        default='/home/ubuntu/code/TheGrandSegmentation/experiment/ea_efficient_fpn/logs/Iria-6-23-12-37-28/fpn101_Iria_1_30.pkl',
-                        help='path to pretrained model')
-    parser.add_argument('--root_dir', nargs='?', type=str, default='/home/ubuntu/data/CityScapes',
-                        help='root dir of dataset')
-    parser.add_argument('--freeze_edge_detection', nargs='?', type=bool, default=True,
-                        help='freeze_dege_detection ')
     args = parser.parse_args()
     train(args)
-    # print(args)
 
 
 
